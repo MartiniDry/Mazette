204ff66756dfcd2309c15ea7f0b4a185400cd635
MartiniDry <71902737+MartiniDry@users.noreply.github.com>
2021-05-26 01:17:06 +0200

    Suite de l'implémentation d'Eller

--- src/com/rosty/maze/model/algorithm/generation/EllerAlgorithm.java
+++ src/com/rosty/maze/model/algorithm/generation/EllerAlgorithm.java
@@ -57,6 +57,7 @@ public class EllerAlgorithm extends MazeGenerationAlgorithm {
 		int i = cellId / nbCol;
 		int j = cellId % nbCol;
 
+		// Assignation d'un nombre pour toutes les cases de la grille
 		if (j == 0) {
 			for (int k = 0; k < nbCol; k++)
 				if (mazePanel.getCell(i, k) == 0)
@@ -64,23 +65,18 @@ public class EllerAlgorithm extends MazeGenerationAlgorithm {
 		}
 
 		if (mode == Mode.EAST_PROCESS) {
-			if (j + 1 < nbCol) {
+			if (j + 1 < nbCol)
 				if (i == 0) {
-					int placeWall = rand.nextInt(2);
-					rightWall(i, j, placeWall);
+					rightWall(i, j, rand.nextInt(2));
 				} else {
-					if (mazePanel.getCell(i, j) == mazePanel.getCell(i, j + 1)) {
-						mazePanel.setWall(i, j, Side.RIGHT, 1);
-					} else {
-						int placeWall = rand.nextInt(2);
-						rightWall(i, j, placeWall);
-					}
+					if (mazePanel.getCell(i, j) == mazePanel.getCell(i, j + 1))
+						rightWall(i, j, 1);
+					else
+						rightWall(i, j, rand.nextInt(2));
 				}
-			}
 		} else if (mode == Mode.SOUTH_PROCESS) {
-			if (i + 1 < nbRow) {
+			if (i + 1 < nbRow)
 				decideForDownWall(i, j);
-			}
 		}
 
 		System.out.println("(" + i + ", " + j + ") -> " + mode.name());
@@ -104,40 +100,51 @@ public class EllerAlgorithm extends MazeGenerationAlgorithm {
 
 	private void rightWall(int i, int j, int value) {
 		mazePanel.setWall(i, j, Side.RIGHT, value);
-		if (value == 0)
-			mazePanel.setCell(i, j + 1, mazePanel.getCell(i, j));
+		if (value == 0) {
+			int before = mazePanel.getCell(i, j);
+			int after = mazePanel.getCell(i, j + 1);
+			int min = Math.min(before, after);
+			int max = Math.max(before, after);
+
+			for (int k = 0; k < nbCol; k++) {
+				if (mazePanel.getCell(i, k) == max)
+					mazePanel.setCell(i, k, min);
+			}
+
+//			mazePanel.setCell(i, j + 1, mazePanel.getCell(i, j));
+		}
 	}
 
 	private void decideForDownWall(int i, int j) {
 		int val = mazePanel.getCell(i, j);
 
 		boolean openedDoor = false;
-//		for (int k = 0; k < j; k++) {
-//			if (mazePanel.getCell(i, k) == val && mazePanel.getWall(i, k, Side.DOWN) == 0)
-//				openedDoor = true;
-//		}
-
-		int k1 = j;
-		while (k1 >= 0 && mazePanel.getWall(i, k1, Side.LEFT) == 0) {
-			if (/* mazePanel.getCell(i, k1) == val && */mazePanel.getWall(i, k1, Side.DOWN) == 0)
+		for (int k = 0; k < j; k++) {
+			if (mazePanel.getCell(i, k) == val && mazePanel.getWall(i, k, Side.DOWN) == 0)
 				openedDoor = true;
-
-			k1--;
 		}
+
+//		int k1 = j;
+//		while (k1 >= 0 && mazePanel.getWall(i, k1, Side.LEFT) == 0) {
+//			if (/* mazePanel.getCell(i, k1) == val && */mazePanel.getWall(i, k1, Side.DOWN) == 0)
+//				openedDoor = true;
+//
+//			k1--;
+//		}
 
 		ArrayList<Integer> array = new ArrayList<>();
-//		for (int k = j + 1; k < nbCol; k++) {
-//			if (mazePanel.getCell(i, k) == val)
-//				array.add(k);
-//		}
-
-		int k2 = j;
-		while (k2 < nbCol && mazePanel.getWall(i, k2, Side.RIGHT) == 0) {
-			if (mazePanel.getCell(i, k2) == val)
-				array.add(k2);
-
-			k2++;
+		for (int k = j + 1; k < nbCol; k++) {
+			if (mazePanel.getCell(i, k) == val)
+				array.add(k);
 		}
+
+//		int k2 = j;
+//		while (k2 < nbCol && mazePanel.getWall(i, k2, Side.RIGHT) == 0) {
+//			if (mazePanel.getCell(i, k2) == val)
+//				array.add(k2);
+//
+//			k2++;
+//		}
 
 		if (array.isEmpty() && !openedDoor) {
 			downWall(i, j, 0);
