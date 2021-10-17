5c75d45c9bb7e6ecb22fe3bf343b78818a98f0c5
MartiniDry <71902737+MartiniDry@users.noreply.github.com>
2021-10-18 01:33:16 +0200

    Refacto for Eller's algorithm

--- src/com/rosty/maze/model/algorithm/generation/EllerAlgorithm.java
+++ src/com/rosty/maze/model/algorithm/generation/EllerAlgorithm.java
@@ -1,6 +1,5 @@
 package com.rosty.maze.model.algorithm.generation;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import com.rosty.maze.model.Maze.Side;
@@ -63,71 +62,23 @@ public class EllerAlgorithm extends MazeGenerationAlgorithm {
 		// Assignation d'un nombre pour toutes les cases de la grille
 		if (j == 0) {
 			// Le mode est "renversé" à chaque début de ligne.
-			mode = ((mode == Mode.EAST_PROCESS) ? Mode.SOUTH_PROCESS : Mode.EAST_PROCESS);
-			
+			mode = (mode == Mode.EAST_PROCESS ? Mode.SOUTH_PROCESS : Mode.EAST_PROCESS);
 			if (mode == Mode.EAST_PROCESS) {
-				if (i == 0) {
-					for (int k = 0; k < nbCol; k++)
-						mazePanel.setCell(i, k, inc++);
-				} else {
-					for (int k = 0; k < nbCol; k++)
-						if (mazePanel.getWall(i, k, Side.UP) == 0)
-							mazePanel.setCell(i, k, mazePanel.getCell(i - 1, k));
-						else
-							mazePanel.setCell(i, k, inc++);
-				}
+				fillWalls(i); // Placement des murs à chaque nouvelle ligne visitée
+				fillCells(i); // Remplissage des cases vides au commencement d'une nouvelle ligne
 			}
 		}
 
-		// Placement des murs à chaque nouvelle ligne visitée
-		if (j == 0) {
-			for (int k = 0; k < nbCol - 1; k++)
-				mazePanel.setWall(i, k, Side.RIGHT, 1);
-
-			if (i < nbRow - 1)
-				for (int k = 0; k < nbCol; k++)
-					mazePanel.setWall(i, k, Side.DOWN, 1);
-		}
-
 		switch (mode) {
-		case EAST_PROCESS:
-			if (j < nbCol - 1)
-				if (i == nbRow - 1) {
-					if (mazePanel.getCell(i, j) != mazePanel.getCell(i, j + 1))
-						mazePanel.setWall(i, j, Side.RIGHT, 1);
-				} else {
-					if (mazePanel.getCell(i, j) != mazePanel.getCell(i, j + 1))
-						mazePanel.setWall(i, j, Side.RIGHT, rand.nextInt(2));
-				}
-
-			break;
-		case SOUTH_PROCESS:
-			int val = rand.nextInt(2);
-			if (val == 0)
-				downWall(i, j, 0);
-
-			break;
-		default:
-			break;
+			case EAST_PROCESS:
+				eastAction(i, j);
+				break;
+			case SOUTH_PROCESS:
+				southAction(i, j);
+				break;
+			default:
+				break;
 		}
-
-//		if (mode == Mode.EAST_PROCESS) {
-//			if (j + 1 < nbCol)
-//				if (i == 0) {
-//					rightWall(i, j, rand.nextInt(2));
-//				} else if (i == nbRow - 1) {
-//					if (mazePanel.getCell(i, j) != mazePanel.getCell(i, j + 1))
-//						rightWall(i, j, 0);
-//				} else {
-//					if (mazePanel.getCell(i, j) == mazePanel.getCell(i, j + 1))
-//						rightWall(i, j, 1);
-//					else
-//						rightWall(i, j, rand.nextInt(2));
-//				}
-//		} else if (mode == Mode.SOUTH_PROCESS) {
-//			if (i + 1 < nbRow)
-//				decideForDownWall(i, j);
-//		}
 
 		System.out.println("(" + i + ", " + j + ") -> " + mode.name());
 
@@ -136,77 +87,72 @@ public class EllerAlgorithm extends MazeGenerationAlgorithm {
 		// - if EAST_PROCESS is active, the current cell goes to the next line.
 		// - if SOUTH_PROCESS is active, the current cell goes back to the beginning of
 		// the line.
+		//
 		// A mode reversing is made too.
 		if (j == nbCol - 1 && i < nbRow - 1) {
-			if (mode == Mode.SOUTH_PROCESS) {
-				cellId += 1;
-			} else if (mode == Mode.EAST_PROCESS) {
+			if (mode == Mode.SOUTH_PROCESS)
+				cellId++;
+			else if (mode == Mode.EAST_PROCESS)
 				cellId -= (nbCol - 1);
-			}
 		} else
 			cellId++;
 	}
 
-	private void rightWall(int i, int j, int value) {
-		mazePanel.setWall(i, j, Side.RIGHT, value);
-		if (value == 0) {
-			int before = mazePanel.getCell(i, j);
-			int after = mazePanel.getCell(i, j + 1);
-			int min = Math.min(before, after);
-			int max = Math.max(before, after);
+	private void fillWalls(int row) {
+		for (int k = 0; k < nbCol - 1; k++)
+			mazePanel.setWall(row, k, Side.RIGHT, 1);
 
-			for (int k = 0; k < nbCol; k++) {
-				if (mazePanel.getCell(i, k) == max)
-					mazePanel.setCell(i, k, min);
+		if (row < nbRow - 1)
+			for (int k = 0; k < nbCol; k++)
+				mazePanel.setWall(row, k, Side.DOWN, 1);
+	}
+
+	private void fillCells(int row) {
+		if (row == 0)
+			for (int k = 0; k < nbCol; k++)
+				mazePanel.setCell(row, k, inc++);
+		else
+			for (int k = 0; k < nbCol; k++)
+				if (mazePanel.getWall(row, k, Side.UP) == 0)
+					mazePanel.setCell(row, k, mazePanel.getCell(row - 1, k));
+				else
+					mazePanel.setCell(row, k, inc++);
+	}
+
+	private void eastAction(int row, int col) {
+		if (col < nbCol - 1) {
+			int val = mazePanel.getCell(row, col);
+			int nextVal = mazePanel.getCell(row, col + 1);
+
+			if (row == nbRow - 1) { // If this is the last grid line, placing walls is completely determined.
+				if (val != nextVal) {
+					mazePanel.setWall(row, col, Side.RIGHT, 0);
+					mazePanel.setCell(row, col + 1, val);
+				}
+			} else {
+				if (rand.nextBoolean() == false) { // If it is decided to break the right wall, ...
+					// ...then remove it and update the current cell value.
+					mazePanel.setWall(row, col, Side.RIGHT, 0);
+					mazePanel.setCell(row, col + 1, val);
+				}
+			}
+		}
+	}
+
+	private void southAction(int row, int col) {
+		if (row < nbRow - 1) {
+			// First case to study is when there is a wall to the right side. Each group of
+			// adjacent cells must be connected to the lower row, so we must be sure at
+			// least one adjacent cell is connected to it.
+			if (mazePanel.getWall(row, col, Side.RIGHT) == 1) {
+				;
 			}
 
-//			mazePanel.setCell(i, j + 1, mazePanel.getCell(i, j));
+			if (rand.nextBoolean() == false) { // If it is decided to break the down wall, ...
+				mazePanel.setWall(row, col, Side.DOWN, 0);
+				mazePanel.setCell(row + 1, col, mazePanel.getCell(row, col));
+			}
 		}
-	}
-
-	private void decideForDownWall(int i, int j) {
-		int val = mazePanel.getCell(i, j);
-
-		boolean openedDoor = false;
-		for (int k = 0; k < j; k++) {
-			if (mazePanel.getCell(i, k) == val && mazePanel.getWall(i, k, Side.DOWN) == 0)
-				openedDoor = true;
-		}
-
-//		int k1 = j;
-//		while (k1 >= 0 && mazePanel.getWall(i, k1, Side.LEFT) == 0) {
-//			if (/* mazePanel.getCell(i, k1) == val && */mazePanel.getWall(i, k1, Side.DOWN) == 0)
-//				openedDoor = true;
-//
-//			k1--;
-//		}
-
-		ArrayList<Integer> array = new ArrayList<>();
-		for (int k = j + 1; k < nbCol; k++) {
-			if (mazePanel.getCell(i, k) == val)
-				array.add(k);
-		}
-
-//		int k2 = j;
-//		while (k2 < nbCol && mazePanel.getWall(i, k2, Side.RIGHT) == 0) {
-//			if (mazePanel.getCell(i, k2) == val)
-//				array.add(k2);
-//
-//			k2++;
-//		}
-
-		if (array.isEmpty() && !openedDoor) {
-			downWall(i, j, 0);
-		} else {
-			int placeWall = rand.nextInt(2);
-			downWall(i, j, placeWall);
-		}
-	}
-
-	private void downWall(int i, int j, int value) {
-		mazePanel.setWall(i, j, Side.DOWN, value);
-		if (value == 0)
-			mazePanel.setCell(i + 1, j, mazePanel.getCell(i, j));
 	}
 
 	private enum Mode {
