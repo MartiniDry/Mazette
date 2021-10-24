e0ec9136ad398eb051ad34b6b4c1528cd606c8f4
MartiniDry <71902737+MartiniDry@users.noreply.github.com>
2021-10-25 00:53:45 +0200

    Ajout du mécanisme de log
    Le chemin du fichier est posé temporairement sur "D:/" mais quelque chose sera mieux fait à l'avenir.

--- src/com/rosty/maze/Mazette.java
+++ src/com/rosty/maze/Mazette.java
@@ -1,5 +1,10 @@
 package com.rosty.maze;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.rosty.maze.application.AppLauncher;
 
 import javafx.application.Application;
@@ -13,11 +18,14 @@ import javafx.application.Platform;
  * @version 1.1
  */
 public class Mazette {
-	/**
-	 * Booléen indiquant si les données de débogage graphique sont affichées sur
-	 * l'IHM.
-	 */
+	/** Journal du logiciel, utilisé pour l'écriture d'événements système. */
+	public static final Logger LOGGER = Logger.getLogger(Mazette.class.getName());
+
+	/** Booléen indiquant si les données de débogage graphique sont affichées. */
 	private static boolean fxDebug = false;
+
+	/** Niveau d'écriture des événements dans le journal de <i>log</i>. */
+	private static Level logLevel = Level.ALL;
 
 	/**
 	 * Méthode principale de l'application <b>Mazette</b> (anciennement <b>Maze
@@ -26,11 +34,36 @@ public class Mazette {
 	 * @param args Paramètres de lancement du logiciel.
 	 */
 	public static void main(String... args) {
+		createLogger();
+
+		LOGGER.info("Playing beep.wav");
 		AppLauncher.playSound("beep.wav");
 
 		for (int id = 0, len = args.length; id < len; id++) {
 			String[] kv = args[id].split("=");
-			if (kv[0].equals("-fxdebug")) {
+			if (kv[0].equals("-loglevel")) {
+				switch (kv[1].toLowerCase()) {
+					case "off":
+					case "severe":
+					case "warning":
+					case "info":
+					case "config":
+					case "fine":
+					case "finer":
+					case "finest":
+					case "all":
+						LOGGER.setLevel(Level.parse(kv[1].toUpperCase()));
+						break;
+					case "debug":
+						LOGGER.setLevel(Level.CONFIG);
+						break;
+					default:
+						LOGGER.setLevel(Level.ALL);
+						break;
+				}
+
+				LOGGER.info("Argument -loglevel: " + logLevel);
+			} else if (kv[0].equals("-fxdebug")) {
 				switch (kv[1]) {
 					case "0":
 					case "false":
@@ -43,6 +76,8 @@ public class Mazette {
 					default:
 						break;
 				}
+
+				LOGGER.info("Argument -fxdebug: " + fxDebug);
 			}
 		}
 
@@ -57,6 +92,18 @@ public class Mazette {
 	 */
 	public static final boolean arg_fxDebug() {
 		return fxDebug;
+	}
+
+	public static final Level arg_logLevel() {
+		return LOGGER.getLevel();
+	}
+
+	public static void createLogger() {
+		try {
+			LOGGER.addHandler(new FileHandler("D:/mazette.log"));
+		} catch (SecurityException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
