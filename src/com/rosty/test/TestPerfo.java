d2783fbcf2aa70b9bf32d6bcffc43a5ee37ced51
MartiniDry <71902737+MartiniDry@users.noreply.github.com>
2022-12-22 22:39:08 +0100

    Nouveaux algos pour le banc de test + grille plus grande

--- src/com/rosty/test/TestPerfo.java
+++ src/com/rosty/test/TestPerfo.java
@@ -6,43 +6,88 @@ import com.rosty.maze.widgets.MazePanel;
 import com.rosty.util.maze.MazeUtils;
 
 public class TestPerfo {
-	public static void main(String[] args) {
-		int row = 400;
-		int col = 600;
-		int tries = 5;
+	public static void main(String[] args) throws InterruptedException {
+		int row = 500;
+		int col = 700;
+		int tries = 30;
 
+		System.out.println("Instanciation Java...");
 		Maze maze = new Maze(row, col);
+		System.out.println("Instanciation JavaFX...");
 		MazePanel panel = new MazePanel();
 		panel.setMaze(maze);
 
+		System.out.println(String.format("Génération d'un labyrinthe (%s,%s)...", row, col));
 		PersonalAlgorithm algo = new PersonalAlgorithm(panel);
 		algo.init();
 		while (!algo.isComplete())
 			algo.step();
 
-		System.out.println("ENCLOS");
+		System.out.println("Pause de 1s pour assurer une vitesse de calcul CPU assez constante.");
+		Thread.sleep(1000);
+
+		System.out.println("ENCLOS (LIGHT)");
 		long tic = System.nanoTime();
 
 		int enc = -1;
 		for (int i = 0; i < tries; i++)
 			enc = MazeUtils.enclosures(maze);
-		
+
 		System.out.println("Il y a " + enc + " enclos !");
 
 		long toc = System.nanoTime();
-		System.out.println("Temps écoulé : " + (toc - tic) / (tries * 1000000000.) + " ms");
+		System.out.println("Temps écoulé : " + (toc - tic) / (tries * 1E6D) + " ms");
 
 		System.out.println();
-		System.out.println("ENCLOS (LIGHT)");
+		System.out.println("ENCLOS");
 		long tic2 = System.nanoTime();
 
 		int enc2 = -1;
 		for (int i = 0; i < tries; i++)
 			enc2 = MazeUtils.enclosures_light(maze);
-		
+
 		System.out.println("Il y a " + enc2 + " enclos !");
 
 		long toc2 = System.nanoTime();
-		System.out.println("Temps écoulé : " + (toc2 - tic2) / (tries * 1000000000.) + " ms");
+		System.out.println("Temps écoulé : " + (toc2 - tic2) / (tries * 1E6D) + " ms");
+
+		System.out.println();
+		System.out.println("ENCLOS (LIGHTER)");
+		long tic3 = System.nanoTime();
+
+		int enc3 = -1;
+		for (int i = 0; i < tries; i++)
+			enc3 = MazeUtils.enclosures_lighter(maze);
+
+		System.out.println("Il y a " + enc3 + " enclos !");
+
+		long toc3 = System.nanoTime();
+		System.out.println("Temps écoulé : " + (toc3 - tic3) / (tries * 1E6D) + " ms");
+
+		System.out.println();
+		System.out.println("ENCLOS (ALTERNATIVE LIGHTER)");
+		long tic4 = System.nanoTime();
+
+		int enc4 = -1;
+		for (int i = 0; i < tries; i++)
+			enc4 = MazeUtils.enclosures_alternative_lighter(maze);
+
+		System.out.println("Il y a " + enc4 + " enclos !");
+
+		long toc4 = System.nanoTime();
+		System.out.println("Temps écoulé : " + (toc4 - tic4) / (tries * 1E6D) + " ms");
+
+		System.out.println();
+		System.out.println("ENCLOS (LIGHTEST)");
+		long tic5 = System.nanoTime();
+
+		int enc5 = -1;
+		for (int i = 0; i < tries; i++)
+			enc5 = MazeUtils.enclosures_lightest(maze);
+
+		System.out.println("Il y a " + enc5 + " enclos !");
+
+		long toc5 = System.nanoTime();
+		System.out.println("Temps écoulé : " + (toc5 - tic5) / (tries * 1E6D) + " ms");
 	}
 }
--- src/com/rosty/util/maze/MazeUtils.java
+++ src/com/rosty/util/maze/MazeUtils.java
@@ -1,5 +1,7 @@
 package com.rosty.util.maze;
 
+import java.util.ArrayList;
+
 import com.rosty.maze.model.Maze;
 import com.rosty.maze.model.Maze.Side;
 
@@ -112,6 +114,159 @@ public class MazeUtils {
 				}
 
 				buffer[j] = cursor;
+			}
+		}
+
+		return addedTokens - deletedTokens;
+	}
+
+	/**
+	 * Version améliorée de la fonction {@link MazeUtils#enclosures(Maze)} qui
+	 * mémorise les données de l'algorithme dans un buffer de la taille d'une ligne.
+	 * On passe ainsi d'une complexité mémoire de O(N²) à O(N), avec ua même
+	 * complexité temporelle.
+	 */
+	public static int enclosures_lighter(Maze maze) {
+		int[] buffer = new int[maze.getNbColumns()];
+		int addedTokens = 0, deletedTokens = 0;
+
+		// Step 1: treating first cell
+		buffer[0] = ++addedTokens;
+
+		// Step 2: treating the rest of the first line
+		for (int j = 1, lenJ = maze.getNbColumns(); j < lenJ; j++)
+			buffer[j] = (maze.getWall(0, j, Side.LEFT) == 1) ? ++addedTokens : buffer[j - 1];
+
+		// Step 3: repeating the process for other lines
+		for (int i = 1, lenI = maze.getNbRows(); i < lenI; i++) {
+			// Step 3.1: treating the left cell
+			if (maze.getWall(i, 0, Side.UP) == 1)
+				buffer[0] = ++addedTokens;
+
+			// Step 3.2: treating the rest of the line
+			for (int j = 1, lenJ = maze.getNbColumns(); j < lenJ; j++) {
+				int upCell = buffer[j];
+				int leftCell = buffer[j - 1];
+				int up = maze.getWall(i, j, Side.UP);
+				int left = maze.getWall(i, j, Side.LEFT);
+
+				if (up != 1 && left != 1 && upCell != leftCell) {
+					for (int k = 0; k < j; k++)
+						if (buffer[k] == leftCell)
+							buffer[k] = upCell;
+
+					deletedTokens++;
+				} else if (up != 1)
+					buffer[j] = upCell;
+				else if (left != 1)
+					buffer[j] = leftCell;
+				else
+					buffer[j] = ++addedTokens;
+			}
+		}
+
+		return addedTokens - deletedTokens;
+	}
+
+	/**
+	 * Version améliorée de la fonction {@link MazeUtils#enclosures(Maze)} qui
+	 * mémorise les données de l'algorithme dans un buffer de la taille d'une ligne.
+	 * On passe ainsi d'une complexité mémoire de O(N²) à O(N), avec ua même
+	 * complexité temporelle.
+	 */
+	public static int enclosures_lightest(Maze maze) {
+		int[] buffer = new int[maze.getNbColumns()];
+		int addedTokens = 0, deletedTokens = 0;
+	
+		// Step 1: treating first cell
+		buffer[0] = ++addedTokens;
+	
+		// Step 2: treating the rest of the first line
+		for (int j = 1, lenJ = maze.getNbColumns(); j < lenJ; j++)
+			buffer[j] = (maze.getWall(0, j, Side.LEFT) == 1) ? ++addedTokens : buffer[j - 1];
+	
+		// Step 3: repeating the process for other lines
+		for (int i = 1, lenI = maze.getNbRows(); i < lenI; i++) {
+			// Step 3.1: treating the left cell
+			if (maze.getWall(i, 0, Side.UP) == 1)
+				buffer[0] = ++addedTokens;
+	
+			// Step 3.2: treating the rest of the line
+			for (int j = 1, lenJ = maze.getNbColumns(); j < lenJ; j++) {
+				int up = maze.getWall(i, j, Side.UP);
+				int left = maze.getWall(i, j, Side.LEFT);
+				
+				if (up == 1) {
+					if (left == 1) {
+						buffer[j] = ++addedTokens;
+					} else {
+						buffer[j] = buffer[j - 1];
+					}
+				} else {
+					int upCell = buffer[j];
+					int leftCell = buffer[j - 1];
+					
+					if (left == 1 || upCell == leftCell) {
+						buffer[j] = upCell;
+					} else {
+						for (int k = 0; k < j; k++)
+							if (buffer[k] == leftCell)
+								buffer[k] = upCell;
+		
+						deletedTokens++;
+					}
+				}
+			}
+		}
+	
+		return addedTokens - deletedTokens;
+	}
+
+	/**
+	 * Version améliorée de la fonction {@link MazeUtils#enclosures(Maze)} qui
+	 * mémorise les données de l'algorithme dans un buffer de la taille d'une ligne.
+	 * On passe ainsi d'une complexité mémoire de O(N²) à O(N), avec ua même
+	 * complexité temporelle.
+	 */
+	public static int enclosures_alternative_lighter(Maze maze) {
+		ArrayList<Integer> buffer = new ArrayList<>();
+		for (int j = 0; j < maze.getNbColumns(); j++)
+			buffer.add(0);
+
+		int addedTokens = 0, deletedTokens = 0;
+
+		// Step 1: treating first cell
+		buffer.set(0, ++addedTokens);
+
+		// Step 2: treating the rest of the first line
+		for (int j = 1, lenJ = maze.getNbColumns(); j < lenJ; j++)
+			buffer.set(j, (maze.getWall(0, j, Side.LEFT) == 1) ? ++addedTokens : buffer.get(j - 1));
+
+		// Step 3: repeating the process for other lines
+		for (int i = 1, lenI = maze.getNbRows(); i < lenI; i++) {
+			// Step 3.1: treating the left cell
+			if (maze.getWall(i, 0, Side.UP) == 1)
+				buffer.set(0, ++addedTokens);
+
+			// Step 3.2: treating the rest of the line
+			for (int j = 1, lenJ = maze.getNbColumns(); j < lenJ; j++) {
+				int upCell = buffer.get(j);
+				int leftCell = buffer.get(j - 1);
+				int up = maze.getWall(i, j, Side.UP);
+				int left = maze.getWall(i, j, Side.LEFT);
+
+				if (up != 1 && left != 1 && upCell != leftCell) {
+					for (int k = 0; k < j; k++)
+						if (buffer.get(k) == leftCell)
+							buffer.set(k, upCell);
+
+					deletedTokens++;
+				} else if (up != 1)
+					buffer.set(j, upCell);
+				else if (left != 1)
+					buffer.set(j, leftCell);
+				else
+					buffer.set(j, ++addedTokens);
 			}
 		}
 
